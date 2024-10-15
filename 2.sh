5cb21cd1ae802756fef363806e0abf65a1204f62
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2024-10-15 17:27:43 -0400

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -40,7 +40,6 @@ sleep 2
 
 flatpak install flathub com.google.Chrome -y
 flatpak install flathub md.obsidian.Obsidian -y
-flatpak install flathub com.dropbox.Client -y
 flatpak install https://flathub.org/beta-repo/appstream/org.gimp.GIMP.flatpakref -y
 flatpak install flathub com.tomjwatson.Emote -y
 flatpak install flathub org.kde.kdenlive -y
