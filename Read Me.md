43bc8e352d8cb9eea61b692b0b639e196cfed57c
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2024-11-22 12:12:06 -0500

    updates

--- Read Me.md
+++ Read Me.md
@@ -4,18 +4,19 @@ This script will automate the install of all the drivers fonts and programs I us
 
 #### *** These scrips must be ran in order. Any deviation will break your system. The "3.sh" "Surface.sh" and "testing.sh" scripts are hardware based/optional. ***
 
-- "1.sh" to install Gnome-shell and a few adjustments.
+- "1.sh" to update and install essentials to configure.
 
 - "2.sh" will install applications, fonts, and cursors (it takes several minutes to run).
 
-- "3.sh" installs the last few Gnome extensions and modify's them. Pop doesnt always like to cooperate. Do not run this script as sudo
+- "3.sh" installs the last few Gnome extensions and modify's them. Pop doesnt always like to cooperate. Do not run this script as sudo and you may have to manually install a few extensions on the list.
 
 - "Surface.sh" will install the necessary drivers to get a Microsoft Surface running on PopOS (Also Debian and Ubuntu have been tested with this same script...skip if not a Surface).
+
+- "popOS-i3wm.sh" will install I3 Window manager and necessary dependancies. 
 
 - If you have multiple hard drives in your system, after you run all the scripts, edit your fstab to auto-mount your drives on boot.
 
 ### Credits:
-- The "usenala" script and "Beautiful Bash" are from https://github.com/ChrisTitusTech/Debian-titus
 - The surface bits are from: https://github.com/linux-surface/linux-surface/wiki but compiled into this script by me.
 
  
---
+++ popOS-i3wm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+# Installing kitty & setting it up
+sudo apt install kitty -y
+sudo update-alternatives --config x-terminal-emulator
+mkdir $HOME/.config/kitty
+cp $HOME/Downloads/i3wm/config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+
+# ulauncher install
+sudo add-apt-repository ppa:agornostal/ulauncher -y
+sudo apt update -y
+sudo apt install ulauncher -y
+
+# Installing required libraries
+sudo apt install libxcb-glx0 libxcb-glx0-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-shape0-dev xcb-proto cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb-composite0-dev python3-xcbgen libxcb-image0-dev libxcb-ewmh-dev libxcb-xrm-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev meson libxext-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-present-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libx11-xcb-dev -y
+
+# Essential Apps
+sudo apt install i3 -y
+sudo apt install polybar -y
+sudo apt install picom -y
+sudo apt install feh numlockx fonts-unifont -y
+sudo apt install neofetch -y
+
+# Replacing files
+cp $HOME/Downloads/i3wm/Wallpapers/bg.jpg $HOME/Pictures/bg.jpg
+mkdir $HOME/.config/i3
+cp $HOME/Downloads/i3wm/config/i3/config $HOME/.config/i3/config
+mkdir $HOME/.config/polybar
+mkdir $HOME/.config/picom
+cp $HOME/Downloads/i3wm/config/polybar/config $HOME/.config/polybar/config
+cp $HOME/Downloads/i3wm/config/polybar/launch.sh $HOME/.config/polybar/launch.sh
+cp $HOME/Downloads/i3wm/config/picom/picom.conf $HOME/.config/picom/picom.conf
