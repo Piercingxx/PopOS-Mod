17d00c8ad7c8ef230114c1111147b2bcae1d84a5
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2024-12-20 06:59:17 -0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -63,6 +63,7 @@ cd "$builddir" || exit
 
 # More Fonts
 mkdir -p $HOME/.fonts
+chown -R "$username":"$username" /home/"$username"/.fonts
 cd $HOME/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip
